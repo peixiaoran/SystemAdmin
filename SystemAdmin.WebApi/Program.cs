511d8f1ea9f3c4dd98d21341a2769ce8e3bd3f2b
xiaoran.pei <3442105608@qq.com>
2026-03-11 19:22:53 +0800

    commit

--- SystemAdmin.WebApi/Program.cs
+++ SystemAdmin.WebApi/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿
+
 using Scalar.AspNetCore;
 using SystemAdmin.CommonSetup.DependencyInjection;
 using SystemAdmin.Hosting.DependencyInjection;
@@ -59,7 +60,35 @@ builder.WebHost.ConfigureKestrel((context, options) =>
 
 var app = builder.Build();
 
-app.MapScalarApiReference("/systemadmin");
+app.MapScalarApiReference("/systemadmin", options =>
+{
+    options.Theme = ScalarTheme.Kepler;
+    options.Layout = ScalarLayout.Modern;
+    options.Title = "SystemAdmin";
+    options.EnabledTargets = new[] { ScalarTarget.CSharp, ScalarTarget.Http };
+
+    options.ShowSidebar = true;
+    options.ShowOperationId = true;
+    options.HideClientButton = true;
+    options.HideModels = true;
+    options.HideDarkModeToggle = false;
+    options.HideTestRequestButton = false;
+    options.HideSearch = false;
+
+    options.ExpandAllModelSections = true;
+    options.ExpandAllResponses = true;
+
+    options.DefaultOpenAllTags = false;
+
+    options.PersistentAuthentication = false;
+    options.Telemetry = true;
+
+    options.WithDocumentDownloadType(DocumentDownloadType.Both);
+    options.WithFavicon("favicon.svg");
+
+    options.OperationTitleSource = OperationTitleSource.Summary;
+    options.DarkMode = false;
+});
 app.MapOpenApi();
 app.UseHttpsRedirection();
 
