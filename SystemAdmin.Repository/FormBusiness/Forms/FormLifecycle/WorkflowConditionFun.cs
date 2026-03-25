5eb3d13634e9e39f27ce99d4e404837447c575a8
xiaoran.pei <3442105608@qq.com>
2026-03-26 00:01:09 +0800

    commit

--- SystemAdmin.Repository/FormBusiness/Forms/FormLifecycle/WorkflowConditionFun.cs
+++ SystemAdmin.Repository/FormBusiness/Forms/FormLifecycle/WorkflowConditionFun.cs
@@ -14,92 +14,47 @@ namespace SystemAdmin.Repository.FormBusiness.Forms.FormLifecycle
         }
 
         /// <summary>
-        /// 请假单-请假人职级范围为师一至师四
+        /// 请假单-请假人职级范围为师一至师五
         /// </summary>
         /// <param name="formId"></param>
         /// <returns></returns>
-        public async Task<bool> LeaveThanPositionDivisionS1_4(long formId)
+        public async Task<bool> LeaveThanPositionDivisionS1_5(long formId)
         {
             return await _db.Queryable<LeaveFormEntity>()
                             .With(SqlWith.NoLock)
                             .InnerJoin<UserInfoEntity>((leave, user) => leave.ApplicantUserId == user.UserId)
                             .InnerJoin<UserPositionEntity>((leave, user, postiton) => user.PositionId == postiton.PositionId)
-                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 12 && postiton.SortOrder <= 15)
+                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 11 && postiton.SortOrder <= 15)
                             .AnyAsync();
         }
 
         /// <summary>
-        /// 请假单-请假人职级范围为师五至师八
+        /// 请假单-请假人职级范围为师六至师十
         /// </summary>
         /// <param name="formId"></param>
         /// <returns></returns>
-        public async Task<bool> LeaveThanPositionDivisionS5_8(long formId)
+        public async Task<bool> LeaveThanPositionDivisionS6_10(long formId)
         {
             return await _db.Queryable<LeaveFormEntity>()
                             .With(SqlWith.NoLock)
                             .InnerJoin<UserInfoEntity>((leave, user) => leave.ApplicantUserId == user.UserId)
                             .InnerJoin<UserPositionEntity>((leave, user, postiton) => user.PositionId == postiton.PositionId)
-                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 8 && postiton.SortOrder <= 11)
+                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 6 && postiton.SortOrder <= 10)
                             .AnyAsync();
         }
 
         /// <summary>
-        /// 请假单-请假人职级范围为师九至师十
+        /// 请假单-请假人职级范围为师十一至师十四
         /// </summary>
         /// <param name="formId"></param>
         /// <returns></returns>
-        public async Task<bool> LeaveThanPositionDivisionS9_10(long formId)
+        public async Task<bool> LeaveThanPositionDivisionS11_14(long formId)
         {
             return await _db.Queryable<LeaveFormEntity>()
                             .With(SqlWith.NoLock)
                             .InnerJoin<UserInfoEntity>((leave, user) => leave.ApplicantUserId == user.UserId)
                             .InnerJoin<UserPositionEntity>((leave, user, postiton) => user.PositionId == postiton.PositionId)
-                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 6 && postiton.SortOrder <= 7)
-                            .AnyAsync();
-        }
-
-        /// <summary>
-        /// 请假单-请假人职级为师十一
-        /// </summary>
-        /// <param name="formId"></param>
-        /// <returns></returns>
-        public async Task<bool> LeaveThanPositionDivisionS11(long formId)
-        {
-            return await _db.Queryable<LeaveFormEntity>()
-                            .With(SqlWith.NoLock)
-                            .InnerJoin<UserInfoEntity>((leave, user) => leave.ApplicantUserId == user.UserId)
-                            .InnerJoin<UserPositionEntity>((leave, user, postiton) => user.PositionId == postiton.PositionId)
-                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder == 5)
-                            .AnyAsync();
-        }
-
-        /// <summary>
-        /// 请假单-请假人职级为师十二
-        /// </summary>
-        /// <param name="formId"></param>
-        /// <returns></returns>
-        public async Task<bool> LeaveThanPositionDivisionS12(long formId)
-        {
-            return await _db.Queryable<LeaveFormEntity>()
-                            .With(SqlWith.NoLock)
-                            .InnerJoin<UserInfoEntity>((leave, user) => leave.ApplicantUserId == user.UserId)
-                            .InnerJoin<UserPositionEntity>((leave, user, postiton) => user.PositionId == postiton.PositionId)
-                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder == 4)
-                            .AnyAsync();
-        }
-
-        /// <summary>
-        /// 请假单-请假人职级范围为师十三至师十四
-        /// </summary>
-        /// <param name="formId"></param>
-        /// <returns></returns>
-        public async Task<bool> LeaveThanPositionDivisionS2_3(long formId)
-        {
-            return await _db.Queryable<LeaveFormEntity>()
-                            .With(SqlWith.NoLock)
-                            .InnerJoin<UserInfoEntity>((leave, user) => leave.ApplicantUserId == user.UserId)
-                            .InnerJoin<UserPositionEntity>((leave, user, postiton) => user.PositionId == postiton.PositionId)
-                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 2 && postiton.SortOrder <= 3)
+                            .Where((leave, user, postiton) => leave.FormId == formId && postiton.SortOrder >= 2 && postiton.SortOrder <= 5)
                             .AnyAsync();
         }
 
